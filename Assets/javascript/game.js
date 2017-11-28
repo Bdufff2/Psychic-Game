ae9be2958334070c6145869e89d2290b322d1cef
duff.brian21@gmail.com <duff.brian21@gmail.com>
2017-11-27 16:12:48 -0800

    game logic functional

--- Assets/javascript/game.js
+++ Assets/javascript/game.js
@@ -1,68 +1,66 @@
 
 
 // list of possible computer selections
-var computerChoices = ["q", "w", "e", "r", "t", "y", "u", "i", "o", "p", "a", "s", "d", "f", "g", "h", "j", "k", "l", "z", "x", "c", "v", "b", "n", "m"];
+var possibleChoices = ["q", "w", "e", "r", "t", "y", "u", "i", "o", "p", "a", "s", "d", "f", "g", "h", "j", "k", "l", "z", "x", "c", "v", "b", "n", "m"];
 
 // list of possible game outcomes
 var wins = 0;
 var losses = 0;
 
-// guesses left and previous guesses
+// guesses left 
 var guessesLeft = 9;
+
+// open array for letters guessed
 var lettersGuessedSoFar = [];
+
+// computer random guess seletion
+var computerGuess = possibleChoices[Math.floor(Math.random() * possibleChoices.length)];
+
+// key the user chooses
+// var userGuess = null;
 
 // user secetion begins the game
 document.onkeyup = function (event) {
 
     // determine which key was pressed
     var userGuess = event.key;
-
-    // computer random guess seletion
-    var computerGuess = computerChoices[Math.floor(math.random() * computerChoices.length)];
-
-
+    // console.log("userGuess", userGuess)
     // possible user guesses / if not selected nothing will happen
-    if ((userGuess === "q") || ((userGuess === "w") || ((userGuess === "e") || ((userGuess === "r") || ((userGuess === "t") || ((userGuess === "y") || ((userGuess === "u") || ((userGuess === "i") || ((userGuess === "o") || ((userGuess === "p") || ((userGuess === "a") || ((userGuess === "s") || ((userGuess === "d") || ((userGuess === "f") || ((userGuess === "g") || ((userGuess === "h") || ((userGuess === "j") || ((userGuess === "k") || ((userGuess === "l") || ((userGuess === "z") || ((userGuess === "x") || ((userGuess === "c") || ((userGuess === "v") || ((userGuess === "b") || ((userGuess === "n") || ((userGuess === "m")) {
+    if (possibleChoices.indexOf(userGuess) >= 0) {
 
-        // calculate wins and loses
-        if (userGuess === computerGuess) {
-            win++;
-        } else if (userGuess != computerGuess) {
-            guessesLeft--;
-            // make wrong guesses appear on screen
-        } else if (userGuess != computerGuess) {
-            "letters Guessed: " + lettersGuessedSoFar;
+        if (lettersGuessedSoFar.indexOf(userGuess) < 0 && possibleChoices.indexOf(userGuess) >= 0) {
+            lettersGuessedSoFar[lettersGuessedSoFar.length] = userGuess;
+            
+
+            // calculate wins and loses
+            if (userGuess === computerGuess) {
+                win++;
+                guessesLeft = 9;
+                lettersGuessedSoFar = [];
+                computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
+            } else {
+                guessesLeft--;
+                // make wrong guesses appear on screen
+                // "letters Guessed: " + lettersGuessedSoFar;
+            }
+
+            // game resets with loss
+            if (guessesLeft === 0) {
+                losses++;
+                guessesLeft = 9;
+                lettersGuessedSoFar = [];
+                computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
+            }
+
+            // keep game updated and display in html
+            var html =
+                "<p>Wins: " + wins + "</p>" +
+                "<p>Losses: " + losses + "</p>" +
+                "<p>Remaining Guesses: " + guessesLeft + "</p>" +
+                "<p>letters Guessed: " + lettersGuessedSoFar + "</p>";
+
+            document.querySelector("#game").innerHTML = html;
+
         }
-
-        if (guessesLeft === 0) {
-            losses++
-        }     }
-
-    
-
-
-    var html =
-        "<p>Wins: " + wins + "</p>" +
-        "<p>Losses: " + losses + "</p>" +
-        "<p>Remaining Guesses: " + guessesLeft + "</p>" +
-        "<p>letters Guessed: " + lettersGuessedSoFar + "</p>" +
-
-
-    document.getElementById("game").innerHTML = html;
-
-
-            // to do's:
-    // need function to reset the game once the correct letter is picked or loss
-    // need to make sure that once a letter is picked it cannot be picked again
-
-
-
-
-
-
+    }
 }
-
-
-
-
-
